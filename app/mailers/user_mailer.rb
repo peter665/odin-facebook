4aafbfa8545032c1db46ae51476a9191dfc3da3f
peter <peto.mester@gmail.com>
2017-06-11 20:27:51 +0200

    Corrected app for deplyoment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome_email user
-    @url = 'http://localhost:3000'
+    @url = Rails.env.development? ? 'http://localhost:3000' : 'https://odin-facebook11.herokuapp.com'
     @user = user
     mail(to: @user.email, subject: 'Welcome to OdinFacebook!')
   end
